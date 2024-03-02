b134ea9a8c0860da8188181d218030ff9f50919c
David Lafontant <lafontant.david@gmail.com>
2024-03-02 13:03:25 -0500

    Create a component to list all the todos (tasks)

---
+++ src/components/todos/Todos.js
@@ -0,0 +1,17 @@
+import React, { useEffect, useState } from 'react'
+import Todo from '../todo/Todo';
+import getAllTodos from '../../utils/getAllTodos';
+
+const Todos = () => {
+  const [todos, setTodos] = useState([]);
+  useEffect(() => { getAllTodos(setTodos) }, [todos])
+  return (
+    <div>
+      <ul>
+        {todos.map(todo => <Todo key={todo.todo_id} todo={todo} />)}
+      </ul>
+    </div>
+  )
+}
+
+export default Todos
