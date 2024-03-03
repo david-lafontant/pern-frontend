5cabffb84ea705d2c034433d610ffb63db97749a
David Lafontant <lafontant.david@gmail.com>
2024-03-03 07:19:25 -0500

    Import EditTodo and displaymodal in Todo component

--- src/components/todo/Todo.js
+++ src/components/todo/Todo.js
@@ -1,15 +1,19 @@
 import React from 'react'
 import Button from '../button/Button';
+import EditTodo from '../editTodo/EditTodo';
 import deleteTodo from '../../utils/deleteTodo';
+import displayModal from '../../utils/toogleModal';
+
 
 const Todo = ({ todo }) => {
   return (
     <li>
+      <EditTodo todo={todo} />
       <span>
         {todo.content}
       </span>
-      <Button type={"button"} text={"Edit"} clickHandle={() => console.log("Edit todo")} />
-      <Button type={"button"} text={"Delete"} clickHandle={(e,) => deleteTodo(e, todo.todo_id)} />
+      <Button type={"button"} text={"Edit"} clickHandle={() => displayModal(todo.todo_id)} />
+      <Button type={"button"} text={"Delete"} clickHandle={(e) => deleteTodo(e, todo.todo_id)} />
     </li>
   )
 }
