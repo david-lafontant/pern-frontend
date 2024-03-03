943df17b889570e8afe6314fcad73516d0daa340
David Lafontant <lafontant.david@gmail.com>
2024-03-03 07:16:50 -0500

    Add functionalities to update a todo in EditTodo component

--- src/components/editTodo/EditTodo.js
+++ src/components/editTodo/EditTodo.js
@@ -1,8 +1,24 @@
-import React from 'react'
+import React, { useState } from 'react'
+import styles from './EditTodo.module.css';
+import Button from '../button/Button';
+import getInputData from '../../utils/getInputData';
+import updateTodo from '../../utils/updateTodo';
 
-const EditTodo = () => {
+const EditTodo = ({ todo }) => {
+  const [editedTodo, setEditedTodo] = useState(todo.content);
+
   return (
-    <div>EditTodo</div>
+    <div className={styles.outer} id={"todo" + todo.todo_id} >
+      <div className='inner'>
+        <p>
+
+          {todo.content}
+        </p>
+        <input type="text" name="editTodo" id="editTodo" onBlur={(e) => getInputData(e, setEditedTodo)} />
+        <Button type={"button"} text={"Edit"} clickHandle={(e) => updateTodo(e, editedTodo, todo.todo_id)} />
+        <p>Edited: {editedTodo}</p>
+      </div>
+    </div>
   )
 }
 
