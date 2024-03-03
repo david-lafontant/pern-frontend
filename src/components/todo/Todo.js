f6f220d80071da20a5f0010eb0b9fb52f7659d6a
David Lafontant <lafontant.david@gmail.com>
2024-03-03 08:14:57 -0500

    Update the import of toggleModal function

--- src/components/todo/Todo.js
+++ src/components/todo/Todo.js
@@ -2,7 +2,7 @@ import React from 'react'
 import Button from '../button/Button';
 import EditTodo from '../editTodo/EditTodo';
 import deleteTodo from '../../utils/deleteTodo';
-import displayModal from '../../utils/toogleModal';
+import toggleModal from '../../utils/toggleModal';
 
 
 const Todo = ({ todo }) => {
@@ -12,7 +12,7 @@ const Todo = ({ todo }) => {
       <span>
         {todo.content}
       </span>
-      <Button type={"button"} text={"Edit"} clickHandle={() => displayModal(todo.todo_id)} />
+      <Button type={"button"} text={"Edit"} clickHandle={() => toggleModal(todo.todo_id)} />
       <Button type={"button"} text={"Delete"} clickHandle={(e) => deleteTodo(e, todo.todo_id)} />
     </li>
   )
